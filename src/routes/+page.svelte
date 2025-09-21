96f36afd7c5381a0945b04f6638dff01baec2371
Lawrence Warren <lawrencewarren2@gmail.com>
2025-09-21 16:25:32 +0100

    Change layout of home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,37 @@
 <script>
-  import img from "$lib/assets/random-header.png";
 </script>
 
-<img src={img} alt="Lawrence Andy and Jonas" />
+<div>
+  <h1>Jonas & Andy</h1>
 
-<h1>Jonas & Andy</h1>
-
-<h2>10th-12th August 2026 - Somerset, UK</h2>
+  <h2>10th-12th August 2026 - Somerset, UK</h2>
+</div>
 
 <p>
-  Ignore the horrifically low resolution image. I just whacked this together and
-  got the framework of the site working. You can see each page (look at the
-  burger menu in the top left) is unique. Basically now I just need to make
-  everything look nice and also have content.
+  Please join us from <b><i>August the 10th until August the 12th 2026</i></b> for
+  the wedding of Jonas Pantaleon and Andrew George.
 </p>
 
 <style>
-  img {
-    width: 100%;
+  div {
+    width: 100vw;
+    height: 50vh;
+    background-image: url("$lib/assets/random-header.png");
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 2rem;
+    font-weight: bold;
+    text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.7);
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  h1 {
+    color: white;
   }
 </style>
